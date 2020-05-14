a84d95877f787c47a8e7e00850e9e4b80169bfa8
rbourdeau1 <63135478+rbourdeau1@users.noreply.github.com>
2020-05-14 00:12:58 -0700

    removed api keys

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "1b3e502042330c8efe8ef1423d4c287a"
+weather_api_key = "Insert WEATHER API KEY HERE"
 
 # Google API Key
-g_key = "AIzaSyD-kdaXnqKviDXMMXhfiZnmVXfKuGRmovg"
+g_key = "Insert GOOGLE API KEY HERE"
